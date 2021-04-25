19dd1c7bf8c4458edc05238007d2ddb1cded0f95
Hector Ccollatupa <hccollatupa@gmail.com>
2021-04-25 18:01:05 -0500

    Promise.race

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-import './styles.css';
+import { promesaLenta, promesaMedia, promesaRapida } from './js/promesas';
+
+/*
+promesaLenta.then(console.log);
+promesaMedia.then(mensaje => console.log(mensaje));
+promesaRapida.then(console.log);
+*/
+
+Promise.race([promesaLenta, promesaMedia, promesaRapida])
+    .then(mensaje => console.log(mensaje))
+    .catch(console.warn);
