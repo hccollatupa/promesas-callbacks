e24248d549718a50836cd1775b7e6b79428587fc
Hector Ccollatupa <hccollatupa@gmail.com>
2021-04-25 17:20:52 -0500

    Callback Hell

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import './styles.css';
 import { buscarHeroe } from './js/callbacks';
 
-const heroeId = 'ironman2';
+const heroeId1 = 'ironman';
+const heroeId2 = 'spiderman';
 
-buscarHeroe(heroeId, (err, heroe) => {
-    if (err) {
-        console.error(err);
-    } else {
-        console.info(heroe);
-    }
+buscarHeroe(heroeId1, (err, heroe1) => {
+    if (err) { return console.error(err); }
+
+    buscarHeroe(heroeId2, (err, heroe2) => {
+        if (err) { return console.error(err); }
+
+        console.log(`Enviando a ${heroe1.nombre} y ${heroe2.nombre} a la misión.`);
+    });
 });
 
 console.log('Fin del programa');
