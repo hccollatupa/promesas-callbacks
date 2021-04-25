e7c2f1831a2bc98e80a19c7bc7648481cae3bfab
Hector Ccollatupa <hccollatupa@gmail.com>
2021-04-25 14:25:58 -0500

    Eliminación de código innecesario

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
-import { saludar } from './js/componentes.js';
 import './styles.css';
-import img from './assets/webpack.png';
 
-const nombre = 'Hector';
-
-saludar(nombre);
+console.log('Hola mundo');
