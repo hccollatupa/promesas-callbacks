223060048a02804912c21b34ab34035b90ee7bd1
Hector Ccollatupa <hccollatupa@gmail.com>
2021-04-25 14:37:39 -0500

    Callbacks

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import './styles.css';
+import { buscarHeroe } from './js/callbacks';
 
-console.log('Hola mundo');
+const heroeId = 'capitanAmerica';
+
+buscarHeroe(heroeId, (heroe) => {
+    console.log(heroe);
+});
---
+++ src/js/callbacks.js
@@ -0,0 +1,20 @@
+const heroes = {
+    ironman: {
+        nombre: 'Ironman',
+        poder: 'Absurda cantidad de dinero'
+    },
+    capitanAmerica: {
+        nombre: 'Capitán América',
+        poder: 'Aguantar inyecciones sin morir'
+    },
+    spiderman: {
+        nombre: 'Spiderman',
+        poder: 'La mejor reacción alergica a las picaduras de arañas'
+    }
+}
+
+//callback retorna un heroe
+export const buscarHeroe = (id, callback) => {
+    const heroe = heroes[id];
+    callback(heroe);
+};
